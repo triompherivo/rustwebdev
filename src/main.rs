317440b28d3e300da33372a8430b32323a0e8af9
Ramandalahy Triomphe <triompherivo@gmail.com>
2022-12-22 19:28:29 +0100

    First commit from the book of Bastian Gruber (CHAPTER 02)

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+use std::io::{Error,ErrorKind};
+use std::str::FromStr;
+
+use warp::Filter;
+
+#[derive(Debug)]
+struct Question {
+    id:QuestionId,
+    title:String,
+    content:String,
+    tags:Option<Vec<String>>,
+}
+#[derive(Debug)]
+struct QuestionId(String);
+
+impl Question {
+    fn new(
+        id:QuestionId,
+        title:String,
+        content:String,
+        tags:Option<Vec<String>>
+    ) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags
+        }
+    }
+}
+
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(
+                Error::new(ErrorKind::InvalidInput,"No id provided")
+            ),
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    /*let question = Question::new(
+        QuestionId::from_str("1").expect("No Id provoded"),
+        "First Question".to_string(),
+        "content of question".to_string(),
+        Some(vec!("faq".to_string())),
+    );
+    println!("{:?}",question);*/
+    let hello = warp::get()
+        .map(|| format!("Hello World"));
+    warp::serve(hello)
+        .run(([127,0,0,1],3030))
+        .await;
+}
